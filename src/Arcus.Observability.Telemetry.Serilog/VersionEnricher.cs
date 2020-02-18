a59ef31a46903a95ee495a86c31f9f83e4fe5f77
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2020-02-18 07:23:34 +0100

    FEAT: add Serilog assembly version enricher

---
+++ src/Arcus.Observability.Telemetry.Serilog/VersionEnricher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Arcus.Observability.Telemetry.Serilog
+{
+    /// <summary>
+    /// Enrichment on log events with the current runtime version (i.e. 'version' = '1.0.0').
+    /// </summary>
+    public class VersionEnricher : ILogEventEnricher
+    {
+        private readonly string _assemblyVersion;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VersionEnricher"/> class.
+        /// </summary>
+        public VersionEnricher()
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            if (executingAssembly == null)
+            {
+                throw new InvalidOperationException(
+                    "Cannot enrich the log events with a 'Version' because the version of the current executing runtime couldn't be determined");
+            }
+            
+            _assemblyVersion = 
+                executingAssembly.GetCustomAttribute<AssemblyVersionAttribute>()?.Version
+                    ?? executingAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+                    ?? executingAssembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version;
+        }
+
+        /// <summary>
+        /// Enrich the log event.
+        /// </summary>
+        /// <param name="logEvent">The log event to enrich.</param>
+        /// <param name="propertyFactory">Factory for creating new properties to add to the event.</param>
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            if (!String.IsNullOrWhiteSpace(_assemblyVersion))
+            {
+                var versionProperty = propertyFactory.CreateProperty("version", _assemblyVersion);
+                logEvent.AddPropertyIfAbsent(versionProperty);
+            }
+        }
+    }
+}
---
+++ src/Arcus.Observability.Tests.Unit/Telemetry/InMemoryLogSink.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Arcus.Observability.Tests.Unit.Telemetry
+{
+    /// <summary>
+    /// Represents a logging sink that collects the emitted log events in-memory.
+    /// </summary>
+    public class InMemoryLogSink : ILogEventSink
+    {
+        private readonly ConcurrentQueue<LogEvent> _logEmits = new ConcurrentQueue<LogEvent>();
+
+        /// <summary>
+        /// Gets the current log emits available on the sink.
+        /// </summary>
+        public IEnumerable<LogEvent> CurrentLogEmits => _logEmits.ToArray();
+
+        /// <summary>
+        /// Emit the provided log event to the sink.
+        /// </summary>
+        /// <param name="logEvent">The log event to write.</param>
+        public void Emit(LogEvent logEvent)
+        {
+            _logEmits.Enqueue(logEvent);
+        }
+    }
+}
