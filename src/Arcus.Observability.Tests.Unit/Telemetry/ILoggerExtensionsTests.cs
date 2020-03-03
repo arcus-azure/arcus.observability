0fc213f85fc7acfff24d4a3f8854a1d846a2f981
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-03-03 14:18:41 +0100

    Provide moar tests

--- src/Arcus.Observability.Tests.Unit/Telemetry/ILoggerExtensionsTests.cs
+++ src/Arcus.Observability.Tests.Unit/Telemetry/ILoggerExtensionsTests.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Globalization;
+using System.Net;
+using System.Net.Http;
 using Arcus.Observability.Telemetry.Core;
 using Bogus;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace Arcus.Observability.Tests.Unit.Telemetry
@@ -39,7 +43,169 @@ namespace Arcus.Observability.Tests.Unit.Telemetry
             double metricValue = _bogusGenerator.Random.Double();
 
             // Act & Arrange
-            Assert.Throws<ArgumentException>(()=> logger.LogMetric(metricName, metricValue));
+            Assert.Throws<ArgumentException>(() => logger.LogMetric(metricName, metricValue));
+        }
+
+        [Fact]
+        public void LogEvent_ValidArguments_Succeeds()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string eventName = _bogusGenerator.Name.FullName();
+
+            // Act
+            logger.LogEvent(eventName);
+
+            // Assert
+            var logMessage = logger.WrittenMessage;
+            Assert.StartsWith(MessagePrefixes.Event, logMessage);
+            Assert.Contains(eventName, logMessage);
+        }
+
+        [Fact]
+        public void LogEvent_NoEventNameSpecified_ThrowsException()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string eventName = null;
+
+            // Act & Arrange
+            Assert.Throws<ArgumentException>(() => logger.LogEvent(eventName));
+        }
+
+        [Fact]
+        public void LogSqlDependency_ValidArguments_Succeeds()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string serverName = _bogusGenerator.Name.FullName();
+            string databaseName = _bogusGenerator.Name.FullName();
+            string tableName = _bogusGenerator.Name.FullName();
+            string operationName = _bogusGenerator.Name.FullName();
+            bool isSuccessful = _bogusGenerator.Random.Bool();
+            DateTimeOffset startTime = _bogusGenerator.Date.PastOffset();
+            var duration = _bogusGenerator.Date.Timespan();
+
+            // Act
+            logger.LogSqlDependency(serverName, databaseName, tableName, operationName, isSuccessful, startTime, duration);
+
+            // Assert
+            var logMessage = logger.WrittenMessage;
+            Assert.StartsWith(MessagePrefixes.DependencyViaSql, logMessage);
+            Assert.Contains(serverName, logMessage);
+            Assert.Contains(databaseName, logMessage);
+            Assert.Contains(tableName, logMessage);
+            Assert.Contains(operationName, logMessage);
+            Assert.Contains(isSuccessful.ToString(), logMessage);
+            Assert.Contains(startTime.ToString(CultureInfo.InvariantCulture), logMessage);
+            Assert.Contains(duration.ToString(), logMessage);
+        }
+
+        [Fact]
+        public void LogSqlDependency_NoServerNameWasSpecified_ThrowsException()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string serverName = null;
+            string databaseName = _bogusGenerator.Name.FullName();
+            string tableName = _bogusGenerator.Name.FullName();
+            string operationName = _bogusGenerator.Name.FullName();
+            bool isSuccessful = _bogusGenerator.Random.Bool();
+            DateTimeOffset startTime = _bogusGenerator.Date.PastOffset();
+            var duration = _bogusGenerator.Date.Timespan();
+
+            // Act & Arrange
+            Assert.Throws<ArgumentException>(() => logger.LogSqlDependency(serverName, databaseName, tableName, operationName, isSuccessful, startTime, duration));
+        }
+
+        [Fact]
+        public void LogSqlDependency_NoDatabaseNameWasSpecified_ThrowsException()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string serverName = _bogusGenerator.Name.FullName();
+            string databaseName = null;
+            string tableName = _bogusGenerator.Name.FullName();
+            string operationName = _bogusGenerator.Name.FullName();
+            bool isSuccessful = _bogusGenerator.Random.Bool();
+            DateTimeOffset startTime = _bogusGenerator.Date.PastOffset();
+            var duration = _bogusGenerator.Date.Timespan();
+
+            // Act & Arrange
+            Assert.Throws<ArgumentException>(() => logger.LogSqlDependency(serverName, databaseName, tableName, operationName, isSuccessful, startTime, duration));
+        }
+
+        [Fact]
+        public void LogSqlDependency_NoTableNameWasSpecified_ThrowsException()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string serverName = _bogusGenerator.Name.FullName();
+            string databaseName = _bogusGenerator.Name.FullName();
+            string tableName = null;
+            string operationName = _bogusGenerator.Name.FullName();
+            bool isSuccessful = _bogusGenerator.Random.Bool();
+            DateTimeOffset startTime = _bogusGenerator.Date.PastOffset();
+            var duration = _bogusGenerator.Date.Timespan();
+
+            // Act & Arrange
+            Assert.Throws<ArgumentException>(() => logger.LogSqlDependency(serverName, databaseName, tableName, operationName, isSuccessful, startTime, duration));
+        }
+
+        [Fact]
+        public void LogSqlDependency_NoOperationNameWasSpecified_ThrowsException()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string serverName = _bogusGenerator.Name.FullName();
+            string databaseName = _bogusGenerator.Name.FullName();
+            string tableName = _bogusGenerator.Name.FullName();
+            string operationName = null;
+            bool isSuccessful = _bogusGenerator.Random.Bool();
+            DateTimeOffset startTime = _bogusGenerator.Date.PastOffset();
+            var duration = _bogusGenerator.Date.Timespan();
+
+            // Act & Arrange
+            Assert.Throws<ArgumentException>(() => logger.LogSqlDependency(serverName, databaseName, tableName, operationName, isSuccessful, startTime, duration));
+        }
+
+        [Fact]
+        public void LogHttpDependency_ValidArguments_Succeeds()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, _bogusGenerator.Internet.Url());
+            HttpStatusCode statusCode = _bogusGenerator.PickRandom<HttpStatusCode>();
+            DateTimeOffset startTime = _bogusGenerator.Date.PastOffset();
+            var duration = _bogusGenerator.Date.Timespan();
+
+            // Act;
+            logger.LogHttpDependency(request, statusCode, startTime, duration);
+
+            // Assert
+            var logMessage = logger.WrittenMessage;
+            Assert.StartsWith(MessagePrefixes.DependencyViaHttp, logMessage);
+            Assert.Contains(request.RequestUri.Host, logMessage);
+            Assert.Contains(request.RequestUri.PathAndQuery, logMessage);
+            Assert.Contains(request.Method.ToString(), logMessage);
+            Assert.Contains(((int)statusCode).ToString(), logMessage);
+            Assert.Contains(startTime.ToString(CultureInfo.InvariantCulture), logMessage);
+            var isSuccessful = (int)statusCode >= 200 && (int)statusCode < 300;
+            Assert.Contains($"Successful: {isSuccessful}", logMessage);
+        }
+
+        [Fact]
+        public void LogHttpDependency_NoRequestWasSpecified_ThrowsException()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            HttpRequestMessage request = null;
+            HttpStatusCode statusCode = _bogusGenerator.PickRandom<HttpStatusCode>();
+            DateTimeOffset startTime = _bogusGenerator.Date.PastOffset();
+            var duration = _bogusGenerator.Date.Timespan();
+
+            // Act & Arrange
+            Assert.Throws<ArgumentException>(() => logger.LogHttpDependency(request, statusCode, startTime, duration));
         }
     }
 }
