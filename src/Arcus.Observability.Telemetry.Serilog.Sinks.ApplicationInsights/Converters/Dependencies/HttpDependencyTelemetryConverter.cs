deddb25753172de1e7cc6539e75903c67a384a9f
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-03-18 16:27:25 +0100

    Provide Application Insights converters, telemetry type filter an… (#40)
    * Provide Application Insights converters, telemetry type filter and configuration extension
    
    * Provide docs
    
    * Reshuffle
    
    * Provide more docs
    
    * Fix references
    
    * Update package name
    
    * Process PR comments
    
    * Update sample
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Fix references
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Fix build failure

---
+++ src/Arcus.Observability.Telemetry.Serilog.Sinks.ApplicationInsights/Converters/Dependencies/HttpDependencyTelemetryConverter.cs
@@ -0,0 +1,8 @@
+﻿namespace Arcus.Observability.Telemetry.Serilog.Sinks.ApplicationInsights.Converters.Dependencies
+{
+    public class HttpDependencyTelemetryConverter : DependencyTelemetryConverter
+    {
+        /// <inheritdoc />
+        protected override DependencyType DependencyType { get; } = DependencyType.Http;
+    }
+}
