3c7bb0c8bef9f4142cb087845281286cb97f4240
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-07-08 10:35:15 +0200

    Feature - clear logger providers except functions

---
+++ src/Arcus.Observability.Telemetry.AzureFunctions/Extensions/ILoggerBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using GuardNet;
+using Microsoft.Extensions.DependencyInjection;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.Logging
+{
+    /// <summary>
+    /// <see cref="ILoggingBuilder"/> extensions related to Azure Functions projects.
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public static class ILoggingBuilderExtensions
+    {
+        /// <summary>
+        /// Clears the <see cref="ILoggerProvider"/> registrations from the given <paramref name="loggingBuilder"/>,
+        /// except the specific Azure Functions registrations.
+        /// </summary>
+        /// <param name="loggingBuilder">The builder containing the <see cref="ILoggerProvider"/> registrations.</param>
+        public static ILoggingBuilder ClearProvidersExceptFunctionProviders(this ILoggingBuilder loggingBuilder)
+        {
+            Guard.NotNull(loggingBuilder, nameof(loggingBuilder));
+
+            // Kudos to katrash: https://stackoverflow.com/questions/45986517/remove-console-and-debug-loggers-in-asp-net-core-2-0-when-in-production-mode
+            foreach (ServiceDescriptor serviceDescriptor in loggingBuilder.Services)
+            {
+                if (serviceDescriptor.ServiceType == typeof(ILoggerProvider))
+                {
+                    if (serviceDescriptor.ImplementationType.FullName != "Microsoft.Azure.WebJobs.Script.Diagnostics.HostFileLoggerProvider"
+                        && serviceDescriptor.ImplementationType.FullName != "Microsoft.Azure.WebJobs.Script.Diagnostics.FunctionFileLoggerProvider")
+                    {
+                        loggingBuilder.Services.Remove(serviceDescriptor);
+                        break;
+                    }
+                }
+            }
+
+            return loggingBuilder;
+        }
+    }
+}
---
+++ src/Arcus.Observability.Tests.Unit/Telemetry/AzureFunctions/Fixture/FunctionFileLoggerProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Azure.WebJobs.Script.Diagnostics
+{
+    /// <summary>
+    /// <see cref="ILoggerProvider"/> to verify if the surrogate of the real provider doesn't get removed.
+    /// </summary>
+    public class FunctionFileLoggerProvider : ILoggerProvider
+    {
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="T:Microsoft.Extensions.Logging.ILogger" /> instance.
+        /// </summary>
+        /// <param name="categoryName">The category name for messages produced by the logger.</param>
+        /// <returns>The instance of <see cref="T:Microsoft.Extensions.Logging.ILogger" /> that was created.</returns>
+        public ILogger CreateLogger(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
