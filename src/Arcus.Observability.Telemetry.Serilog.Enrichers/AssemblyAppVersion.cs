0a2168c2e225706c1a155dd82966940f6f651a71
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-09-25 14:25:31 +0200

    Feature - inject custom application version in Serilog version enricher (#144)

---
+++ src/Arcus.Observability.Telemetry.Serilog.Enrichers/AssemblyAppVersion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace Arcus.Observability.Telemetry.Serilog.Enrichers
+{
+    /// <summary>
+    /// Represents an <see cref="IAppVersion"/> implementation that uses the assembly version as application version.
+    /// </summary>
+    /// <seealso cref="IAppVersion"/>
+    public class AssemblyAppVersion : IAppVersion
+    {
+        private readonly Lazy<string> _assemblyVersion;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssemblyAppVersion"/> class.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when the process executable in the default application domain cannot be retrieved.</exception>
+        public AssemblyAppVersion()
+        {
+            var executingAssembly = Assembly.GetEntryAssembly();
+            if (executingAssembly is null)
+            {
+                throw new InvalidOperationException(
+                    "Cannot enrich the log events with a 'Version' because the version of the current executing runtime couldn't be determined");
+            }
+            
+            _assemblyVersion = new Lazy<string>(() => 
+                executingAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+                    ?? executingAssembly.GetCustomAttribute<AssemblyVersionAttribute>()?.Version
+                    ?? executingAssembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version);
+        }
+
+        /// <summary>
+        /// Gets the current version of the application.
+        /// </summary>
+        public string GetVersion()
+        {
+            return _assemblyVersion.Value;
+        }
+    }
+}
--- src/Arcus.Observability.Tests.Unit/Serilog/VersionEnricherTests.cs
+++ src/Arcus.Observability.Tests.Unit/Serilog/VersionEnricherTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Arcus.Observability.Telemetry.Serilog.Enrichers;
 using Arcus.Observability.Tests.Core;
+using Moq;
 using Serilog;
 using Serilog.Events;
 using Xunit;
@@ -56,6 +57,80 @@ namespace Arcus.Observability.Tests.Unit.Serilog
             Assert.True(Version.TryParse(value.ToString().Trim('\"'), out Version result));
         }
 
+        [Fact]
+        public void LogEvent_WithCustomAppVersion_HasCustomVersion()
+        {
+            // Arrange
+            var spy = new InMemoryLogSink();
+            string expected = $"version-{Guid.NewGuid()}";
+            var stubAppVersion = new Mock<IAppVersion>();
+            stubAppVersion.Setup(v => v.GetVersion()).Returns(expected);
+            ILogger logger =
+                new LoggerConfiguration()
+                    .WriteTo.Sink(spy)
+                    .Enrich.WithVersion(stubAppVersion.Object)
+                    .CreateLogger();
+
+            // Act
+            logger.Information("This log event will be enriched with a custom application version");
+
+            // Assert
+            LogEvent emit = Assert.Single(spy.CurrentLogEmits);
+            Assert.NotNull(emit);
+            (string key, LogEventPropertyValue value) = Assert.Single(emit.Properties);
+            Assert.Equal(VersionEnricher.DefaultPropertyName, key);
+            Assert.Equal(expected, value.ToDecentString());
+        }
+
+        [Fact]
+        public void LogEvent_WithCustomAppVersionAndCustomPropertyName_HasCustomVersion()
+        {
+            // Arrange
+            var spy = new InMemoryLogSink();
+            string propertyName = $"version-name-{Guid.NewGuid()}";
+            string expected = $"version-{Guid.NewGuid()}";
+            var stubAppVersion = new Mock<IAppVersion>();
+            stubAppVersion.Setup(v => v.GetVersion()).Returns(expected);
+            ILogger logger =
+                new LoggerConfiguration()
+                    .WriteTo.Sink(spy)
+                    .Enrich.WithVersion(stubAppVersion.Object, propertyName)
+                    .CreateLogger();
+
+            // Act
+            logger.Information("This log event will be enriched with a custom application version on a custom property");
+
+            // Assert
+            LogEvent emit = Assert.Single(spy.CurrentLogEmits);
+            Assert.NotNull(emit);
+            (string key, LogEventPropertyValue value) = Assert.Single(emit.Properties);
+            Assert.Equal(propertyName, key);
+            Assert.Equal(expected, value.ToDecentString());
+        }
+
+        [Theory]
+        [ClassData(typeof(Blanks))]
+        public void LogEvent_WithCustomAppVersionReturningBlankVersion_DoesntAddVersionProperty(string version)
+        {
+            // Arrange
+            var spy = new InMemoryLogSink();
+            var stubAppVersion = new Mock<IAppVersion>();
+            stubAppVersion.Setup(v => v.GetVersion()).Returns(version);
+            ILogger logger =
+                new LoggerConfiguration()
+                    .WriteTo.Sink(spy)
+                    .Enrich.WithVersion(stubAppVersion.Object)
+                    .CreateLogger();
+
+            // Act
+            logger.Information("This log event will not be enriched with an custom application version because the version is blank");
+
+            // Assert
+            LogEvent emit = Assert.Single(spy.CurrentLogEmits);
+            Assert.NotNull(emit);
+            Assert.Empty(emit.Properties);
+        }
+
         [Theory]
         [ClassData(typeof(Blanks))]
         public void WithVersion_WithBlankPropertyName_Throws(string propertyName)
@@ -69,9 +144,67 @@ namespace Arcus.Observability.Tests.Unit.Serilog
 
         [Theory]
         [ClassData(typeof(Blanks))]
-        public void CreatesEnricher_WithBlankPropertyName_Throws(string propertyName)
+        public void WithVersion_WithAppVersionAndBlankPropertyName_Throws(string propertyName)
+        {
+            // Arrange
+            var configuration = new LoggerConfiguration();
+            var appVersion = Mock.Of<IAppVersion>();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(() => configuration.Enrich.WithVersion(appVersion, propertyName));
+        }
+
+        [Fact]
+        public void WithVersion_WithoutAppVersion_Throws()
+        {
+            // Arrange
+            var configuration = new LoggerConfiguration();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(
+                () => configuration.Enrich.WithVersion(appVersion: null));
+        }
+
+        [Fact]
+        public void WithVersion_WithoutAppVersionAndPropertyName_Throws()
+        {
+            // Arrange
+            var configuration = new LoggerConfiguration();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(
+                () => configuration.Enrich.WithVersion(appVersion: null, propertyName: "some-property-name"));
+        }
+
+        [Theory]
+        [ClassData(typeof(Blanks))]
+        public void CreateEnricher_WithBlankPropertyName_Throws(string propertyName)
         {
             Assert.ThrowsAny<ArgumentException>(() => new VersionEnricher(propertyName));
+        }
+
+        [Theory]
+        [ClassData(typeof(Blanks))]
+        public void CreateEnricher_WithAppVersionAndBlankPropertyName_Throws(string propertyName)
+        {
+            // Arrange
+            var appVersion = Mock.Of<IAppVersion>();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(() => new VersionEnricher(appVersion, propertyName));
+        }
+
+        [Fact]
+        public void CreateEnricher_WithoutAppVersion_Throws()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => new VersionEnricher(appVersion: null));
+        }
+
+        [Fact]
+        public void CreateEnricher_WithoutAppVersionAndCustomPropertyName_Throws()
+        {
+            Assert.ThrowsAny<ArgumentException>(
+                () => new VersionEnricher(appVersion: null, propertyName: "some-property-name"));
         }
     }
 }
