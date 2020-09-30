e64ffaa7ec231f78d53dbb23b4dc9801caeacb68
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-09-30 11:56:56 +0200

    Feature - register the IAppVersion as service

---
+++ src/Arcus.Observability.Tests.Unit/Telemetry/DummyAppVersion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Arcus.Observability.Telemetry.Serilog.Enrichers;
+
+namespace Arcus.Observability.Tests.Unit.Telemetry
+{
+    /// <summary>
+    /// Dummy <see cref="IAppVersion"/> implementation.
+    /// </summary>
+    public class DummyAppVersion : IAppVersion
+    {
+        /// <summary>
+        /// Gets the current version of the application.
+        /// </summary>
+        public string GetVersion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Arcus.Observability.Tests.Unit/Telemetry/StubAppVersion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Arcus.Observability.Telemetry.Serilog.Enrichers;
+
+namespace Arcus.Observability.Tests.Unit.Telemetry
+{
+    /// <summary>
+    /// Stubbed implementation of the <see cref="IAppVersion"/>.
+    /// </summary>
+    public class StubAppVersion : IAppVersion
+    {
+        private readonly string _version;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StubAppVersion"/> class.
+        /// </summary>
+        /// <param name="version">The current stubbed application version.</param>
+        public StubAppVersion(string version)
+        {
+            _version = version;
+        }
+
+        /// <summary>
+        /// Gets the current version of the application.
+        /// </summary>
+        public string GetVersion()
+        {
+            return _version;
+        }
+    }
+}
