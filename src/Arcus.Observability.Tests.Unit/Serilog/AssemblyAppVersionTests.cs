304961915113a1f18be1480232067fdb30fcc624
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-11-04 11:24:15 +0100

    pr-fix: use correct namespace

--- src/Arcus.Observability.Tests.Unit/Serilog/AssemblyAppVersionTests.cs
+++ src/Arcus.Observability.Tests.Unit/Serilog/AssemblyAppVersionTests.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Arcus.Observability.Telemetry.Serilog.Enrichers;
+using Arcus.Observability.Telemetry.Core;
 using Xunit;
 
 namespace Arcus.Observability.Tests.Unit.Serilog
--- src/Arcus.Observability.Tests.Unit/Telemetry/IServiceCollectionExtensionsTests.cs
+++ src/Arcus.Observability.Tests.Unit/Telemetry/IServiceCollectionExtensionsTests.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Arcus.Observability.Telemetry.Serilog.Enrichers;
+using Arcus.Observability.Telemetry.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
