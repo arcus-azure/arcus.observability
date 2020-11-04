715f9b6d4f8a6a15ee8fd2c7c260a8cf415cc675
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-11-04 08:57:16 +0100

    Provide capability to only specifiy status code for LogRequest instead of full response

--- src/Arcus.Observability.Telemetry.Core/Extensions/ILoggerExtensions.cs
+++ src/Arcus.Observability.Telemetry.Core/Extensions/ILoggerExtensions.cs
@@ -117,6 +117,30 @@ namespace Microsoft.Extensions.Logging
         /// </summary>
         /// <param name="logger">Logger to use</param>
         /// <param name="request">Request that was done</param>
+        /// <param name="responseStatusCode">HTTP status code returned by the service</param>
+        /// <param name="duration">Duration of the operation</param>
+        /// <param name="context">Context that provides more insights on the HTTP request that was tracked</param>
+        public static void LogRequest(this ILogger logger, HttpRequest request, int responseStatusCode, TimeSpan duration, Dictionary<string, object> context = null)
+        {
+            Guard.NotNull(logger, nameof(logger));
+            Guard.NotNull(request, nameof(request));
+            Guard.For<ArgumentException>(() => request.Scheme != null && request.Scheme.Contains(" "), "HTTP request scheme cannot contain whitespace");
+            Guard.For<ArgumentException>(() => !request.Host.HasValue, "HTTP request host requires a value");
+            Guard.For<ArgumentException>(() => request.Host.ToString()?.Contains(" ") == true, "HTTP request host name cannot contain whitespace");
+
+            context = context ?? new Dictionary<string, object>();
+
+            PathString resourcePath = request.Path;
+            string host = $"{request.Scheme}://{request.Host}";
+
+            logger.LogInformation(RequestFormat, request.Method, host, resourcePath, responseStatusCode, duration, DateTimeOffset.UtcNow.ToString(CultureInfo.InvariantCulture), context);
+        }
+
+        /// <summary>
+        ///     Logs an HTTP request
+        /// </summary>
+        /// <param name="logger">Logger to use</param>
+        /// <param name="request">Request that was done</param>
         /// <param name="response">Response that will be sent out</param>
         /// <param name="duration">Duration of the operation</param>
         /// <param name="context">Context that provides more insights on the HTTP request that was tracked</param>
@@ -131,7 +155,32 @@ namespace Microsoft.Extensions.Logging
 
             context = context ?? new Dictionary<string, object>();
 
-            var statusCode = (int) response.StatusCode;
+            var statusCode = (int)response.StatusCode;
+            PathString resourcePath = request.RequestUri.AbsolutePath;
+            string host = $"{request.RequestUri.Scheme}://{request.RequestUri.Host}";
+
+            logger.LogInformation(RequestFormat, request.Method, host, resourcePath, statusCode, duration, DateTimeOffset.UtcNow.ToString(CultureInfo.InvariantCulture), context);
+        }
+
+        /// <summary>
+        ///     Logs an HTTP request
+        /// </summary>
+        /// <param name="logger">Logger to use</param>
+        /// <param name="request">Request that was done</param>
+        /// <param name="responseStatusCode">HTTP status code returned by the service</param>
+        /// <param name="duration">Duration of the operation</param>
+        /// <param name="context">Context that provides more insights on the HTTP request that was tracked</param>
+        public static void LogRequest(this ILogger logger, HttpRequestMessage request, HttpStatusCode responseStatusCode, TimeSpan duration, Dictionary<string, object> context = null)
+        {
+            Guard.NotNull(logger, nameof(logger));
+            Guard.NotNull(request, nameof(request));
+            Guard.NotNull(request.RequestUri, nameof(request.RequestUri));
+            Guard.For<ArgumentException>(() => request.RequestUri.Scheme?.Contains(" ") == true, "HTTP request scheme cannot contain whitespace");
+            Guard.For<ArgumentException>(() => request.RequestUri.Host?.Contains(" ") == true, "HTTP request host name cannot contain whitespace");
+
+            context = context ?? new Dictionary<string, object>();
+
+            var statusCode = (int)responseStatusCode;
             PathString resourcePath = request.RequestUri.AbsolutePath;
             string host = $"{request.RequestUri.Scheme}://{request.RequestUri.Host}";
 
--- src/Arcus.Observability.Tests.Unit/Telemetry/ILoggerExtensionsTests.cs
+++ src/Arcus.Observability.Tests.Unit/Telemetry/ILoggerExtensionsTests.cs
@@ -1001,7 +1001,7 @@ namespace Arcus.Observability.Tests.Unit.Telemetry
         }
 
         [Fact]
-        public void LogRequest_ValidArguments_Succeeds()
+        public void LogRequest_ValidArgumentsIncludingResponse_Succeeds()
         {
             // Arrange
             var logger = new TestLogger();
@@ -1030,7 +1030,34 @@ namespace Arcus.Observability.Tests.Unit.Telemetry
         }
 
         [Fact]
-        public void LogRequestMessage_ValidArguments_Succeeds()
+        public void LogRequest_ValidArgumentsIncludingResponseStatusCode_Succeeds()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            var statusCode = (int)_bogusGenerator.PickRandom<HttpStatusCode>();
+            var path = $"/{_bogusGenerator.Lorem.Word()}";
+            var host = _bogusGenerator.Lorem.Word();
+            var method = HttpMethod.Head;
+            var mockRequest = new Mock<HttpRequest>();
+            mockRequest.Setup(request => request.Method).Returns(method.ToString());
+            mockRequest.Setup(request => request.Host).Returns(new HostString(host));
+            mockRequest.Setup(request => request.Path).Returns(path);
+            var duration = _bogusGenerator.Date.Timespan();
+
+            // Act
+            logger.LogRequest(mockRequest.Object, statusCode, duration);
+
+            // Assert
+            var logMessage = logger.WrittenMessage;
+            Assert.StartsWith(MessagePrefixes.RequestViaHttp, logMessage);
+            Assert.Contains(path, logMessage);
+            Assert.Contains(host, logMessage);
+            Assert.Contains(statusCode.ToString(), logMessage);
+            Assert.Contains(method.ToString(), logMessage);
+        }
+
+        [Fact]
+        public void LogRequestMessage_ValidArgumentsIncludingResponse_Succeeds()
         {
             // Arrange
             var logger = new TestLogger();
@@ -1050,7 +1077,31 @@ namespace Arcus.Observability.Tests.Unit.Telemetry
             Assert.StartsWith(MessagePrefixes.RequestViaHttp, logMessage);
             Assert.Contains(path, logMessage);
             Assert.Contains(host, logMessage);
-            Assert.Contains(((int) statusCode).ToString(), logMessage);
+            Assert.Contains(((int)statusCode).ToString(), logMessage);
+            Assert.Contains(method.ToString(), logMessage);
+        }
+
+        [Fact]
+        public void LogRequestMessage_ValidArgumentsIncludingResponseStatusCode_Succeeds()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            var statusCode = _bogusGenerator.PickRandom<HttpStatusCode>();
+            var path = $"/{_bogusGenerator.Name.FirstName().ToLower()}";
+            var host = _bogusGenerator.Name.FirstName().ToLower();
+            var method = HttpMethod.Head;
+            var request = new HttpRequestMessage(method, new Uri("https://" + host + path));
+            var duration = _bogusGenerator.Date.Timespan();
+
+            // Act;
+            logger.LogRequest(request, statusCode, duration);
+
+            // Assert
+            var logMessage = logger.WrittenMessage;
+            Assert.StartsWith(MessagePrefixes.RequestViaHttp, logMessage);
+            Assert.Contains(path, logMessage);
+            Assert.Contains(host, logMessage);
+            Assert.Contains(((int)statusCode).ToString(), logMessage);
             Assert.Contains(method.ToString(), logMessage);
         }
 
