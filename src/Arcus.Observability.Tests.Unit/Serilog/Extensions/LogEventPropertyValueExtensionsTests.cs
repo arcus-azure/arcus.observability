634f73acb873efa2258da3a9bb17e35ea42e2dbe
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-05-07 14:33:54 +0200

    pr-fix: add missing unit tests for 'GetAsEnum' update

--- src/Arcus.Observability.Tests.Unit/Serilog/Extensions/LogEventPropertyValueExtensionsTests.cs
+++ src/Arcus.Observability.Tests.Unit/Serilog/Extensions/LogEventPropertyValueExtensionsTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Arcus.Observability.Telemetry.Core;
+using Bogus;
 using Serilog.Events;
 using Xunit;
 
@@ -8,21 +10,24 @@ namespace Arcus.Observability.Tests.Unit.Serilog.Extensions
 {
     public class LogEventPropertyValueExtensionsTests
     {
-        [Fact]
-        public void GetAsStructureValue_WithoutPropertyKey_Fails()
+        private readonly Faker _bogusGenerator = new Faker();
+        
+        [Theory]
+        [ClassData(typeof(Blanks))]
+        public void GetAsStructureValue_WithoutPropertyKey_Fails(string propertyKey)
         {
             // Arrange
             var properties = new ReadOnlyDictionary<string, LogEventPropertyValue>(
                 new Dictionary<string, LogEventPropertyValue>());
             
             // Act / Assert
-            Assert.ThrowsAny<ArgumentException>(() => properties.GetAsStructureValue(propertyKey: null));
+            Assert.ThrowsAny<ArgumentException>(() => properties.GetAsStructureValue(propertyKey));
         }
 
         [Fact]
         public void GetAsStructureValue_WithFoundPropertyWithoutAssociatedStructureValue_Succeeds()
         {
-            string propertyKey = $"prop-{Guid.NewGuid()}";
+            string propertyKey = _bogusGenerator.Random.Word();
             var properties = new ReadOnlyDictionary<string, LogEventPropertyValue>(
                 new Dictionary<string, LogEventPropertyValue>
                 {
@@ -41,7 +46,7 @@ namespace Arcus.Observability.Tests.Unit.Serilog.Extensions
         public void GetAsStructureValue_WithFoundPropertyKeyAssociatedWithStructureValue_Succeeds()
         {
             // Arrange
-            string propertyKey = $"prop-{Guid.NewGuid()}";
+            string propertyKey = _bogusGenerator.Random.Word();
             var expected = new StructureValue(new LogEventProperty[0]);
             var properties = new ReadOnlyDictionary<string, LogEventPropertyValue>(
                 new Dictionary<string, LogEventPropertyValue>
@@ -54,6 +59,70 @@ namespace Arcus.Observability.Tests.Unit.Serilog.Extensions
             
             // Assert
             Assert.Same(expected, actual);
+        }
+
+        [Theory]
+        [ClassData(typeof(Blanks))]
+        public void GetAsEnum_WithoutPropertyKey_Fails(string propertyKey)
+        {
+            // Arrange
+            var properties = new ReadOnlyDictionary<string, LogEventPropertyValue>(
+                new Dictionary<string, LogEventPropertyValue>());
+            
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(() => properties.GetAsEnum<TelemetryType>(propertyKey));
+        }
+
+        [Fact]
+        public void GetAsEnum_WithoutPropertyValue_ReturnsNull()
+        {
+            // Arrange
+            string propertyKey = _bogusGenerator.Random.Word();
+            var properties = new ReadOnlyDictionary<string, LogEventPropertyValue>(
+                new Dictionary<string, LogEventPropertyValue>
+                {
+                    [propertyKey] = null
+                });
+            
+            // Act
+            TelemetryType? result = properties.GetAsEnum<TelemetryType>(propertyKey);
+            
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void GetAsEnum_WithoutParsablePropertyValue_Fails()
+        {
+            // Arrange
+            string propertyKey = _bogusGenerator.Random.Word();
+            var properties = new ReadOnlyDictionary<string, LogEventPropertyValue>(
+                new Dictionary<string, LogEventPropertyValue>
+                {
+                    [propertyKey] = new ScalarValue(_bogusGenerator.Random.Word())
+                });
+            
+            // Act / Assert
+            Assert.ThrowsAny<FormatException>(() => properties.GetAsEnum<TelemetryType>(propertyKey));
+        }
+        
+        [Fact]
+        public void GetAsEnum_WithParsablePropertyValue_Succeeds()
+        {
+            // Arrange
+            string propertyKey = _bogusGenerator.Random.Word();
+            TelemetryType expected = _bogusGenerator.Random.Enum<TelemetryType>();
+            var properties = new ReadOnlyDictionary<string, LogEventPropertyValue>(
+                new Dictionary<string, LogEventPropertyValue>
+                {
+                    [propertyKey] = new ScalarValue(expected)
+                });
+            
+            // Act
+            TelemetryType? actual = properties.GetAsEnum<TelemetryType>(propertyKey);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
