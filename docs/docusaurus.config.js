7760a1a61517f5a2fb88c9d0b75df3493cdd5e06
Michael Calewaert <michael.calewaert@codit.eu>
2021-10-08 16:09:37 +0200

    :memo: Add search

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -12,7 +12,15 @@ module.exports = {
   organizationName: 'arcus-azure', // Usually your GitHub org/user name.
   projectName: 'Arcus - Observability', // Usually your repo name.
   themeConfig: {
-    sidebarCollapsible: false,
+    algolia: {
+      apiKey: 'fdbac718aeb246ac60aeae5bd286cbbf',
+      indexName: 'arcus-azure',
+      // Set `contextualSearch` to `true` when having multiple versions!!!
+      contextualSearch: true,
+      searchParameters: {
+        facetFilters: ["tags:observability"]
+      },
+    },
     image: 'img/arcus.jpg',
     navbar: {
       title: 'Observability',
@@ -87,6 +95,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: "/",
           path: 'preview',
+          sidebarCollapsible: false,
           // Please change this to your repo.
           editUrl:
             'https://github.com/arcus-azure/arcus.observability/edit/master/docs',
