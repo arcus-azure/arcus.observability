af6f4bd9ad1c41868a587f52960ca2124e41a78b
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2022-01-06 11:29:27 +0100

    fix: use serilog scalar value when serializing string values (#262)
    * bug: use serilog scalar value when serializing string values
    
    * pr-del: remove comment-out code

---
+++ src/Arcus.Observability.Tests.Unit/Fixture/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Arcus.Observability.Tests.Unit.Fixture
+{
+    public class Order
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("orderNumber")]
+        public int OrderNumber { get; set; }
+
+        [JsonPropertyName("scheduled")]
+        public DateTimeOffset Scheduled { get; set; }
+    }
+}
