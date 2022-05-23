10a926ea45ac2a0e84e284e727db02b46117ea0e
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-05-23 09:13:57 +0200

    feat: add extension to rm microsoft appinsights logprovider

---
+++ src/Arcus.Observability.Tests.Integration/Serilog/Sinks/ApplicationInsights/AzureFunctionsDockerTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.ApplicationInsights.Query;
+using Microsoft.Azure.ApplicationInsights.Query.Models;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Arcus.Observability.Tests.Integration.Serilog.Sinks.ApplicationInsights
+{
+    [Collection("Docker")]
+    [Trait("Category", "Docker")]
+    public class AzureFunctionsDockerTests : ApplicationInsightsSinkTests
+    {
+        private static readonly HttpClient HttpClient = new HttpClient();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AzureFunctionsDockerTests" /> class.
+        /// </summary>
+        public AzureFunctionsDockerTests(ITestOutputHelper outputWriter) 
+            : base(outputWriter)
+        {
+        }
+
+        [Fact]
+        public async Task LogRequest_WithRequestsOperationName_SinksToApplicationInsights()
+        {
+            // Arrange
+            int httpPort = Configuration.GetValue<int>("AzureFunctions:HttpPort");
+            using (HttpResponseMessage response = await HttpClient.GetAsync($"http://localhost:{httpPort}/api/order"))
+            {
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+                using (ApplicationInsightsDataClient client = CreateApplicationInsightsClient())
+                {
+                    await RetryAssertUntilTelemetryShouldBeAvailableAsync(async () =>
+                    {
+                        EventsResults<EventsRequestResult> results = await client.Events.GetRequestEventsAsync(ApplicationId, PastHalfHourTimeSpan);
+                        Assert.NotEmpty(results.Value);
+                        AssertX.Any(results.Value, result =>
+                        {
+                            Assert.Contains("order", result.Request.Url);
+                            Assert.Equal(200.ToString(), result.Request.ResultCode);
+                            Assert.True(Guid.TryParse(result.Request.Id, out Guid _));
+                            Assert.Equal(HttpMethod.Get.Method + " /api/order", result.Operation.Name);
+                        });
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ src/Arcus.Observability.Tests.Runtimes.AzureFunction/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arcus.Observability.Tests.Runtimes.AzureFunctions;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Events;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace Arcus.Observability.Tests.Runtimes.AzureFunctions
+{
+    internal class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            builder.ConfigurationBuilder.AddEnvironmentVariables();
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = builder.GetContext().Configuration;
+
+            var instrumentationKey = config.GetValue<string>("APPINSIGHTS_INSTRUMENTATIONKEY");
+
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.AzureApplicationInsights(instrumentationKey)
+                .CreateLogger();
+
+            builder.Services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.RemoveMicrosoftApplicationInsightsLoggerProvider()
+                              .AddSerilog(logger);
+            });
+        }
+    }
+}
