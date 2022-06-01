28c3ccd1a2849082b1e2e0dc841d0fdd6ec44793
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2022-06-01 10:06:32 +0200

    feat: add dependency id to cosmosdb dep tracking (#380)
    * feat: add dependency id to cosmosdb dep tracking
    
    * pr-sug: add better description for duration measurement
    
    * pr-fix: use 'duration' io 'latency' for duration measurement description

--- src/Arcus.Observability.Tests.Unit/Telemetry/Logging/CosmosSqlDependencyLoggingTests.cs
+++ src/Arcus.Observability.Tests.Unit/Telemetry/Logging/CosmosSqlDependencyLoggingTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Arcus.Observability.Telemetry.Core;
 using Arcus.Observability.Telemetry.Core.Logging;
 using Bogus;
@@ -10,19 +11,19 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
     [Trait("Category", "Unit")]
     public class CosmosSqlDependencyLoggingTests
     {
-        private readonly Faker _bogusGenerator = new Faker();
+        private static readonly Faker BogusGenerator = new Faker();
 
         [Fact]
         public void LogCosmosSqlDependency_ValidArguments_Succeeds()
         {
             // Arrange
             var logger = new TestLogger();
-            string container = _bogusGenerator.Commerce.ProductName();
-            string database = _bogusGenerator.Commerce.ProductName();
-            string accountName = _bogusGenerator.Finance.AccountName();
-            bool isSuccessful = _bogusGenerator.Random.Bool();
-            DateTimeOffset startTime = _bogusGenerator.Date.PastOffset();
-            TimeSpan duration = _bogusGenerator.Date.Timespan();
+            string container = BogusGenerator.Commerce.ProductName();
+            string database = BogusGenerator.Commerce.ProductName();
+            string accountName = BogusGenerator.Finance.AccountName();
+            bool isSuccessful = BogusGenerator.Random.Bool();
+            DateTimeOffset startTime = BogusGenerator.Date.PastOffset();
+            TimeSpan duration = BogusGenerator.Date.Timespan();
 
             // Act
             logger.LogCosmosSqlDependency(accountName, database, container, isSuccessful, startTime, duration);
@@ -41,15 +42,47 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
         }
 
         [Fact]
+        public void LogCosmosSqlDependencyWithDependencyId_ValidArguments_Succeeds()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string container = BogusGenerator.Lorem.Word();
+            string database = BogusGenerator.Lorem.Word();
+            string accountName = BogusGenerator.Lorem.Word();
+            bool isSuccessful = BogusGenerator.Random.Bool();
+            DateTimeOffset startTime = BogusGenerator.Date.PastOffset();
+            TimeSpan duration = BogusGenerator.Date.Timespan();
+            string dependencyId = BogusGenerator.Lorem.Word();
+
+            string key = BogusGenerator.Lorem.Word();
+            string value = BogusGenerator.Lorem.Word();
+            var context = new Dictionary<string, object> { [key] = value };
+
+            // Act
+            logger.LogCosmosSqlDependency(accountName, database, container, isSuccessful, startTime, duration, dependencyId, context);
+
+            // Assert
+            DependencyLogEntry dependency = logger.GetMessageAsDependency();
+            Assert.Equal("Azure DocumentDB", dependency.DependencyType);
+            Assert.Equal(accountName, dependency.TargetName);
+            Assert.Equal($"{database}/{container}", dependency.DependencyName);
+            Assert.Equal($"{database}/{container}", dependency.DependencyData);
+            Assert.Equal(startTime.ToString(FormatSpecifiers.InvariantTimestampFormat), dependency.StartTime);
+            Assert.Equal(duration, dependency.Duration);
+            Assert.Equal(dependencyId, dependency.DependencyId);
+            Assert.Equal(value, Assert.Contains(key, dependency.Context));
+        }
+
+        [Fact]
         public void LogCosmosSqlDependency_WithNegativeDuration_Fails()
         {
             // Arrange
             var logger = new TestLogger();
-            string container = _bogusGenerator.Commerce.ProductName();
-            string database = _bogusGenerator.Commerce.ProductName();
-            string accountName = _bogusGenerator.Finance.AccountName();
-            bool isSuccessful = _bogusGenerator.Random.Bool();
-            DateTimeOffset startTime = _bogusGenerator.Date.PastOffset();
+            string container = BogusGenerator.Commerce.ProductName();
+            string database = BogusGenerator.Commerce.ProductName();
+            string accountName = BogusGenerator.Finance.AccountName();
+            bool isSuccessful = BogusGenerator.Random.Bool();
+            DateTimeOffset startTime = BogusGenerator.Date.PastOffset();
             TimeSpan duration = TimeSpanGenerator.GeneratePositiveDuration().Negate();
 
             // Act / Assert
@@ -57,14 +90,31 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
         }
 
         [Fact]
+        public void LogCosmosSqlDependencyWithDependencyId_WithNegativeDuration_Fails()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string container = BogusGenerator.Commerce.ProductName();
+            string database = BogusGenerator.Commerce.ProductName();
+            string accountName = BogusGenerator.Finance.AccountName();
+            bool isSuccessful = BogusGenerator.Random.Bool();
+            DateTimeOffset startTime = BogusGenerator.Date.PastOffset();
+            TimeSpan duration = TimeSpanGenerator.GeneratePositiveDuration().Negate();
+            string dependencyId = BogusGenerator.Lorem.Word();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(() => logger.LogCosmosSqlDependency(accountName, database, container, isSuccessful, startTime, duration, dependencyId));
+        }
+
+        [Fact]
         public void LogCosmosSqlDependencyWithDependencyMeasurement_ValidArguments_Succeeds()
         {
             // Arrange
             var logger = new TestLogger();
-            string container = _bogusGenerator.Commerce.ProductName();
-            string database = _bogusGenerator.Commerce.ProductName();
-            string accountName = _bogusGenerator.Finance.AccountName();
-            bool isSuccessful = _bogusGenerator.Random.Bool();
+            string container = BogusGenerator.Commerce.ProductName();
+            string database = BogusGenerator.Commerce.ProductName();
+            string accountName = BogusGenerator.Finance.AccountName();
+            bool isSuccessful = BogusGenerator.Random.Bool();
 
             var measurement = DependencyMeasurement.Start();
             DateTimeOffset startTime = measurement.StartTime;
@@ -92,10 +142,10 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
         {
             // Arrange
             var logger = new TestLogger();
-            string container = _bogusGenerator.Lorem.Word();
-            string database = _bogusGenerator.Lorem.Word();
-            string accountName = _bogusGenerator.Lorem.Word();
-            bool isSuccessful = _bogusGenerator.Random.Bool();
+            string container = BogusGenerator.Lorem.Word();
+            string database = BogusGenerator.Lorem.Word();
+            string accountName = BogusGenerator.Lorem.Word();
+            bool isSuccessful = BogusGenerator.Random.Bool();
 
             var measurement = DurationMeasurement.Start();
             DateTimeOffset startTime = measurement.StartTime;
@@ -116,15 +166,51 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
             Assert.Equal(isSuccessful, dependency.IsSuccessful);
         }
 
+        [Fact]
+        public void LogCosmosSqlDependencyWithDurationMeasurementWithDependencyId_ValidArguments_Succeeds()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string container = BogusGenerator.Lorem.Word();
+            string database = BogusGenerator.Lorem.Word();
+            string accountName = BogusGenerator.Lorem.Word();
+            bool isSuccessful = BogusGenerator.Random.Bool();
+
+            var measurement = DurationMeasurement.Start();
+            DateTimeOffset startTime = measurement.StartTime;
+            measurement.Dispose();
+            TimeSpan duration = measurement.Elapsed;
+            string dependencyId = BogusGenerator.Lorem.Word();
+
+            string key = BogusGenerator.Lorem.Word();
+            string value = BogusGenerator.Lorem.Word();
+            var context = new Dictionary<string, object> { [key] = value };
+
+            // Act
+            logger.LogCosmosSqlDependency(accountName, database, container, isSuccessful, measurement, dependencyId, context);
+
+            // Assert
+            DependencyLogEntry dependency = logger.GetMessageAsDependency();
+            Assert.Equal(accountName, dependency.TargetName);
+            Assert.Equal("Azure DocumentDB", dependency.DependencyType);
+            Assert.Equal(database + "/" + container, dependency.DependencyName);
+            Assert.Equal(database + "/" + container, dependency.DependencyData);
+            Assert.Equal(startTime.ToString(FormatSpecifiers.InvariantTimestampFormat), dependency.StartTime);
+            Assert.Equal(duration, dependency.Duration);
+            Assert.Equal(isSuccessful, dependency.IsSuccessful);
+            Assert.Equal(dependencyId, dependency.DependencyId);
+            Assert.Equal(value, Assert.Contains(key, dependency.Context));
+        }
+
         [Theory]
         [ClassData(typeof(Blanks))]
         public void LogCosmosSqlDependencyWithDurationMeasurement_WithoutAccountName_Fails(string accountName)
         {
             // Arrange
             var logger = new TestLogger();
-            string container = _bogusGenerator.Commerce.ProductName();
-            string database = _bogusGenerator.Commerce.ProductName();
-            bool isSuccessful = _bogusGenerator.Random.Bool();
+            string container = BogusGenerator.Commerce.ProductName();
+            string database = BogusGenerator.Commerce.ProductName();
+            bool isSuccessful = BogusGenerator.Random.Bool();
 
             var measurement = DurationMeasurement.Start();
             measurement.Dispose();
@@ -132,6 +218,25 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
             // Act / Assert
             Assert.ThrowsAny<ArgumentException>(
                 () => logger.LogCosmosSqlDependency(accountName, database, container, isSuccessful, measurement));
+        }
+
+        [Theory]
+        [ClassData(typeof(Blanks))]
+        public void LogCosmosSqlDependencyWithDurationMeasurementWithDependencyId_WithoutAccountName_Fails(string accountName)
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string container = BogusGenerator.Commerce.ProductName();
+            string database = BogusGenerator.Commerce.ProductName();
+            bool isSuccessful = BogusGenerator.Random.Bool();
+
+            var measurement = DurationMeasurement.Start();
+            measurement.Dispose();
+            string dependencyId = BogusGenerator.Lorem.Word();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(
+                () => logger.LogCosmosSqlDependency(accountName, database, container, isSuccessful, measurement, dependencyId));
         }
 
         [Theory]
@@ -140,9 +245,9 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
         {
             // Arrange
             var logger = new TestLogger();
-            string container = _bogusGenerator.Commerce.ProductName();
-            string accountName = _bogusGenerator.Finance.AccountName();
-            bool isSuccessful = _bogusGenerator.Random.Bool();
+            string container = BogusGenerator.Commerce.ProductName();
+            string accountName = BogusGenerator.Finance.AccountName();
+            bool isSuccessful = BogusGenerator.Random.Bool();
 
             var measurement = DurationMeasurement.Start();
             measurement.Dispose();
@@ -154,13 +259,32 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
 
         [Theory]
         [ClassData(typeof(Blanks))]
+        public void LogCosmosSqlDependencyWithDurationMeasurementWithDependencyId_WithoutDatabase_Fails(string database)
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string container = BogusGenerator.Commerce.ProductName();
+            string accountName = BogusGenerator.Finance.AccountName();
+            bool isSuccessful = BogusGenerator.Random.Bool();
+
+            var measurement = DurationMeasurement.Start();
+            measurement.Dispose();
+            string dependencyId = BogusGenerator.Lorem.Word();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(
+                () => logger.LogCosmosSqlDependency(accountName, database, container, isSuccessful, measurement, dependencyId));
+        }
+
+        [Theory]
+        [ClassData(typeof(Blanks))]
         public void LogCosmosSqlDependencyWithDurationMeasurement_WithoutContainer_Fails(string container)
         {
             // Arrange
             var logger = new TestLogger();
-            string database = _bogusGenerator.Commerce.ProductName();
-            string accountName = _bogusGenerator.Finance.AccountName();
-            bool isSuccessful = _bogusGenerator.Random.Bool();
+            string database = BogusGenerator.Commerce.ProductName();
+            string accountName = BogusGenerator.Finance.AccountName();
+            bool isSuccessful = BogusGenerator.Random.Bool();
 
             var measurement = DurationMeasurement.Start();
             measurement.Dispose();
@@ -170,15 +294,34 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
                 () => logger.LogCosmosSqlDependency(accountName, database, container, isSuccessful, measurement));
         }
 
+        [Theory]
+        [ClassData(typeof(Blanks))]
+        public void LogCosmosSqlDependencyWithDurationMeasurementWithDependencyId_WithoutContainer_Fails(string container)
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string database = BogusGenerator.Commerce.ProductName();
+            string accountName = BogusGenerator.Finance.AccountName();
+            bool isSuccessful = BogusGenerator.Random.Bool();
+
+            var measurement = DurationMeasurement.Start();
+            measurement.Dispose();
+            string dependencyId = BogusGenerator.Lorem.Word();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(
+                () => logger.LogCosmosSqlDependency(accountName, database, container, isSuccessful, measurement, dependencyId));
+        }
+
         [Fact]
         public void LogCosmosSqlDependencyWithDurationMeasurement_WithoutMeasurement_Fails()
         {
             // Arrange
             var logger = new TestLogger();
-            string database = _bogusGenerator.Commerce.ProductName();
-            string container = _bogusGenerator.Commerce.ProductName();
-            string accountName = _bogusGenerator.Finance.AccountName();
-            bool isSuccessful = _bogusGenerator.Random.Bool();
+            string database = BogusGenerator.Commerce.ProductName();
+            string container = BogusGenerator.Commerce.ProductName();
+            string accountName = BogusGenerator.Finance.AccountName();
+            bool isSuccessful = BogusGenerator.Random.Bool();
 
             var measurement = DurationMeasurement.Start();
             measurement.Dispose();
@@ -186,6 +329,25 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
             // Act / Assert
             Assert.ThrowsAny<ArgumentException>(
                 () => logger.LogCosmosSqlDependency(accountName, database, container, isSuccessful, measurement: (DurationMeasurement)null));
+        }
+
+        [Fact]
+        public void LogCosmosSqlDependencyWithDurationMeasurementWithDependencyId_WithoutMeasurement_Fails()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string database = BogusGenerator.Commerce.ProductName();
+            string container = BogusGenerator.Commerce.ProductName();
+            string accountName = BogusGenerator.Finance.AccountName();
+            bool isSuccessful = BogusGenerator.Random.Bool();
+
+            var measurement = DurationMeasurement.Start();
+            measurement.Dispose();
+            string dependencyId = BogusGenerator.Lorem.Word();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(
+                () => logger.LogCosmosSqlDependency(accountName, database, container, isSuccessful, measurement: null, dependencyId));
         }
     }
 }
