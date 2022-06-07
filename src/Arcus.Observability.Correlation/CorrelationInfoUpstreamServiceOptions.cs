26bf626fe91710dc0475eb88a96ce765b3eecd05
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2022-06-07 11:33:15 +0200

    feat: add upstream service correlation options (#386)
    * feat: add upstream service correlation options
    
    * pr-sug: add tests and better property name to avoid conflicts

---
+++ src/Arcus.Observability.Correlation/CorrelationInfoUpstreamServiceOptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using GuardNet;
+
+namespace Arcus.Observability.Correlation
+{
+    /// <summary>
+    /// Correlation options specific to the upstream services, used in the <see cref="CorrelationInfoOptions"/>.
+    /// </summary>
+    public class CorrelationInfoUpstreamServiceOptions
+    {
+        private string _operationParentIdHeaderName = "Request-Id";
+
+        /// <summary>
+        /// Gets or sets the flag indicating whether or not the upstream service information should be extracted from the <see cref="OperationParentIdHeaderName"/> following the W3C Trace-Context standard. 
+        /// </summary>
+        public bool ExtractFromRequest { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets the request header name where te operation parent ID is located (default: <c>"Request-Id"</c>).
+        /// </summary>
+        /// <exception cref="ArgumentException">Thrown when the <paramref name="value"/> is blank.</exception>
+        public string OperationParentIdHeaderName
+        {
+            get => _operationParentIdHeaderName;
+            set
+            {
+                Guard.NotNullOrWhitespace(value, nameof(value), "Requires a non-blank value for the operation parent ID request header name");
+                _operationParentIdHeaderName = value;
+            }
+        }
+    }
+}
---
+++ src/Arcus.Observability.Tests.Unit/Correlation/CorrelationInfoOptionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Arcus.Observability.Correlation;
+using Xunit;
+
+namespace Arcus.Observability.Tests.Unit.Correlation
+{
+    [Trait("Category", "Unit")]
+    public class CorrelationInfoOptionsTests
+    {
+        [Fact]
+        public void OperationParentIdPropertyName_HasDefault_Succeeds()
+        {
+            // Arrange
+            var options = new CorrelationInfoOptions();
+
+            // Act / Assert
+            Assert.NotEmpty(options.OperationParent.OperationParentIdHeaderName);
+        }
+
+        [Theory]
+        [ClassData(typeof(Blanks))]
+        public void SetOperationParentIdPropertyName_WithBlankValue_Fails(string operationIdPropertyName)
+        {
+            // Arrange
+            var options = new CorrelationInfoOptions();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(() =>
+                options.OperationParent.OperationParentIdHeaderName = operationIdPropertyName);
+        }
+    }
+}
