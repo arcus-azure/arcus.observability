d576537595e68fb8aeae6eaaeed88e69b712d5e0
Vincent ter Maat <Vincent.terMaat@codit.eu>
2022-07-04 12:15:08 +0200

    Add unit-tests

--- src/Arcus.Observability.Tests.Unit/Telemetry/Logging/MetricLoggingTests.cs
+++ src/Arcus.Observability.Tests.Unit/Telemetry/Logging/MetricLoggingTests.cs
@@ -22,6 +22,28 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
             double metricValue = _bogusGenerator.Random.Double();
 
             // Act
+            logger.LogMetric(metricName, metricValue);
+
+            // Assert
+            MetricLogEntry metric = logger.GetMessageAsMetric();
+            Assert.Equal(metricName, metric.MetricName);
+            Assert.Equal(metricValue, metric.MetricValue);
+            Assert.Collection(metric.Context, item =>
+            {
+                Assert.Equal(ContextProperties.General.TelemetryType, item.Key);
+                Assert.Equal(TelemetryType.Metrics, item.Value);
+            });
+        }
+
+        [Fact]
+        public void LogCustomMetric_ValidArguments_Succeeds()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string metricName = _bogusGenerator.Name.FullName();
+            double metricValue = _bogusGenerator.Random.Double();
+
+            // Act
             logger.LogCustomMetric(metricName, metricValue);
 
             // Assert
@@ -37,6 +59,30 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
 
         [Fact]
         public void LogMetric_ValidArgumentsWithTimestamp_Succeeds()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string metricName = _bogusGenerator.Lorem.Word();
+            double metricValue = _bogusGenerator.Random.Double();
+            DateTimeOffset timestamp = _bogusGenerator.Date.RecentOffset();
+
+            // Act
+            logger.LogMetric(metricName, metricValue, timestamp);
+
+            // Assert
+            MetricLogEntry metric = logger.GetMessageAsMetric();
+            Assert.Equal(metricName, metric.MetricName);
+            Assert.Equal(metricValue, metric.MetricValue);
+            Assert.Equal(timestamp.ToString(FormatSpecifiers.InvariantTimestampFormat), metric.Timestamp);
+            Assert.Collection(metric.Context, item =>
+            {
+                Assert.Equal(ContextProperties.General.TelemetryType, item.Key);
+                Assert.Equal(TelemetryType.Metrics, item.Value);
+            });
+        }
+
+        [Fact]
+        public void LogCustomMetric_ValidArgumentsWithTimestamp_Succeeds()
         {
             // Arrange
             var logger = new TestLogger();
@@ -73,6 +119,40 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
             var context = new Dictionary<string, object> { [key] = value };
 
             // Act
+            logger.LogMetric(metricName, metricValue, timestamp, context);
+
+            // Assert
+            MetricLogEntry metric = logger.GetMessageAsMetric();
+            Assert.Equal(metricName, metric.MetricName);
+            Assert.Equal(metricValue, metric.MetricValue);
+            Assert.Equal(timestamp.ToString(FormatSpecifiers.InvariantTimestampFormat), metric.Timestamp);
+            Assert.Collection(metric.Context,
+                item =>
+                {
+                    Assert.Equal(key, item.Key);
+                    Assert.Equal(value, item.Value);
+                },
+                item =>
+                {
+                    Assert.Equal(ContextProperties.General.TelemetryType, item.Key);
+                    Assert.Equal(TelemetryType.Metrics, item.Value);
+                });
+        }
+
+        [Fact]
+        public void LogCustomMetric_ValidArgumentsWithCustomContext_Succeeds()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string metricName = _bogusGenerator.Lorem.Word();
+            double metricValue = _bogusGenerator.Random.Double();
+            DateTimeOffset timestamp = _bogusGenerator.Date.RecentOffset();
+
+            string key = _bogusGenerator.Lorem.Word();
+            string value = _bogusGenerator.Lorem.Word();
+            var context = new Dictionary<string, object> { [key] = value };
+
+            // Act
             logger.LogCustomMetric(metricName, metricValue, timestamp, context);
 
             // Assert
@@ -95,6 +175,18 @@ namespace Arcus.Observability.Tests.Unit.Telemetry.Logging
 
         [Fact]
         public void LogMetric_NoMetricNameWasSpecified_ThrowsException()
+        {
+            // Arrange
+            var logger = new TestLogger();
+            string metricName = null;
+            double metricValue = _bogusGenerator.Random.Double();
+
+            // Act & Arrange
+            Assert.Throws<ArgumentException>(() => logger.LogMetric(metricName, metricValue));
+        }
+
+        [Fact]
+        public void LogCustomMetric_NoMetricNameWasSpecified_ThrowsException()
         {
             // Arrange
             var logger = new TestLogger();
