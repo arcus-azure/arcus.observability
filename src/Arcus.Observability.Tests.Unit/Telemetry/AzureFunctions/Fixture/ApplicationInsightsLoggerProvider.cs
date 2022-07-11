3b9718f1710d0646ed951a84a87ff22ff5d878ce
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-07-11 07:31:40 +0200

    test: add unit test for remove micro app insights extension

---
+++ src/Arcus.Observability.Tests.Unit/Telemetry/AzureFunctions/Fixture/ApplicationInsightsLoggerProvider.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Arcus.Observability.Tests.Unit.Telemetry.AzureFunctions.Fixture
+{
+    /// <summary>
+    /// Represents a dummy class with the expected name to be removed by the <see cref="ILoggingBuilderExtensions.RemoveMicrosoftApplicationInsightsLoggerProvider"/>.
+    /// </summary>
+    public class ApplicationInsightsLoggerProvider
+    {
+    }
+}
