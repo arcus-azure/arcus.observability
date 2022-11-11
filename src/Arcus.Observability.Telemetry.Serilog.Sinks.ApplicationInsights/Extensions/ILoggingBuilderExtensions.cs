07159c740715349c0acd4c82756481b3ff11c57e
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2022-11-11 07:19:10 +0100

    feat: add 'AddSerilog' extension overload with 'IServiceProvider' (#482)
    * feat: add 'AddSerilog' extension overload with 'IServiceProvider'
    
    * Update src/Arcus.Observability.Telemetry.Serilog.Sinks.ApplicationInsights/Extensions/ILoggingBuilderExtensions.cs
    
    Co-authored-by: Frederik Gheysels <frederik.gheysels@telenet.be>
    
    Co-authored-by: Frederik Gheysels <frederik.gheysels@telenet.be>

---
+++ src/Arcus.Observability.Telemetry.Serilog.Sinks.ApplicationInsights/Extensions/ILoggingBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GuardNet;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog.Extensions.Logging;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.Logging
+{
+    /// <summary>
+    /// Extensions on the <see cref="ILoggingBuilder"/>
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public static class ILoggingBuilderExtensions
+    {
+        /// <summary>
+        /// Add Serilog to the logging pipeline.
+        /// </summary>
+        /// <param name="builder">The <see cref="ILoggingBuilder" /> to add logging provider to.</param>
+        /// <param name="implementationFactory">The factory function to create an Serilog logger implementation.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> or <paramref name="implementationFactory"/> is <c>null</c>.</exception>
+        public static ILoggingBuilder AddSerilog(
+            this ILoggingBuilder builder,
+            Func<IServiceProvider, Serilog.ILogger> implementationFactory)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a logging builder instance to add the Serilog logger provider");
+            Guard.NotNull(implementationFactory, nameof(implementationFactory), "Requires an implementation factory to build up the Serilog logger");
+
+            builder.Services.AddSingleton<ILoggerProvider>(provider =>
+            {
+                return new SerilogLoggerProvider(implementationFactory(provider), dispose: true);
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ src/Arcus.Observability.Tests.Unit/Serilog/ILoggingBuilderExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Arcus.Observability.Correlation;
+using Arcus.Observability.Tests.Unit.Telemetry.AzureFunctions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
+using Xunit;
+
+namespace Arcus.Observability.Tests.Unit.Serilog
+{
+    // ReSharper disable once InconsistentNaming
+    public class ILoggingBuilderExtensionsTests
+    {
+        [Fact]
+        public void AddSerilog_WithImplementationFactory_IncludesSerilogLogger()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddCorrelation();
+
+            // Act
+            services.AddLogging(builder => builder.AddSerilog(provider =>
+            {
+                var accessor = provider.GetRequiredService<ICorrelationInfoAccessor>();
+                return new LoggerConfiguration()
+                       .Enrich.WithCorrelationInfo(accessor)
+                       .WriteTo.Console()
+                       .CreateLogger();
+            }));
+
+            // Assert
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+            var loggerProvider = serviceProvider.GetService<ILoggerProvider>();
+            Assert.NotNull(loggerProvider);
+            Assert.IsType<SerilogLoggerProvider>(loggerProvider);
+            Assert.NotNull(serviceProvider.GetService<ILogger<ILoggerBuilderExtensionsTests>>());
+        }
+
+        [Fact]
+        public void AddSerilog_WithoutImplementationFactory_Fails()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(
+                () => services.AddLogging(builder => builder.AddSerilog(implementationFactory: null)));
+        }
+    }
+}
