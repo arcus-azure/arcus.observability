7a85565b5b4f1f88bded0586189eec942279efc9
Laurens - Codit BE <laurens.vancauwelaert@codit.eu>
2024-04-24 15:29:31 +0200

    no explicit search depd

--- docs/docusaurus.build.config.js
+++ docs/docusaurus.build.config.js
@@ -4,15 +4,15 @@ module.exports = {
   ...buildConfig,
   themeConfig: {
     ...buildConfig.themeConfig,
-    // algolia: {
-    //   appId: process.env.ALGOLIA_APP_ID,
-    //   apiKey: process.env.ALGOLIA_API_KEY,
-    //   indexName: 'arcus-azure',
-    //   // Set `contextualSearch` to `true` when having multiple versions!!!
-    //   contextualSearch: true,
-    //   searchParameters: {
-    //     facetFilters: ["tags:observability"]
-    //   },
-    // },
+    algolia: {
+      appId: process.env.ALGOLIA_APP_ID,
+      apiKey: process.env.ALGOLIA_API_KEY,
+      indexName: 'arcus-azure',
+      // Set `contextualSearch` to `true` when having multiple versions!!!
+      contextualSearch: true,
+      searchParameters: {
+        facetFilters: ['tags:observability'],
+      },
+    },
   },
 };
