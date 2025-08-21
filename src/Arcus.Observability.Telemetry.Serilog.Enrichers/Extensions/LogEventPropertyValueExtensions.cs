5c135e6d4dc7177a1f73cae3d02b8128e30ace62
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-08-21 10:13:40 +0200

    chore(core): apply code-factor suggestions

--- src/Arcus.Observability.Telemetry.Serilog.Enrichers/Extensions/LogEventPropertyValueExtensions.cs
+++ src/Arcus.Observability.Telemetry.Serilog.Enrichers/Extensions/LogEventPropertyValueExtensions.cs
@@ -11,7 +11,7 @@ namespace Serilog.Events
     /// </summary>
     public static class LogEventPropertyValueExtensions
     {
-        private static readonly StructureValue EmptyStructureValue = new StructureValue(new LogEventProperty[0]);
+        private static readonly StructureValue EmptyStructureValue = new StructureValue(Array.Empty<LogEventProperty>());
 
         /// <summary>
         ///     Provide a string representation for a property key
@@ -218,12 +218,12 @@ namespace Serilog.Events
             else
             {
                 string propertyValueAsString = logEventPropertyValue.ToString().Trim();
-                if (propertyValueAsString.StartsWith("\""))
+                if (propertyValueAsString.StartsWith('\"'))
                 {
                     propertyValueAsString = propertyValueAsString.Remove(0, 1);
                 }
 
-                if (propertyValueAsString.EndsWith("\""))
+                if (propertyValueAsString.EndsWith('\"'))
                 {
                     propertyValueAsString = propertyValueAsString.Remove(propertyValueAsString.Length - 1);
                 }
